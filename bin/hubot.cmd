83309354d14cba7d61750e1255983cba6e3bbb9c
sergeyt <stodyshev@gmail.com>
2015-07-21 16:04:01 +0600

    start using campfire-adapter

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,4 +1,9 @@
 @echo off
 set HUBOT_LOG_LEVEL=debug
+set HUBOT_CAMPFIRE_HOST=localhost
+set HUBOT_CAMPFIRE_PORT=3000
+set HUBOT_CAMPFIRE_APIPREFIX=api
+set HUBOT_CAMPFIRE_TOKEN=hubot:hub0t2015
+set HUBOT_CAMPFIRE_ACCOUNT=hubot
 
-npm install && node_modules\.bin\hubot.cmd --name "hubot" --adapter adapter %*
+npm install && node_modules\.bin\hubot.cmd --name "hubot" --adapter "campfire-adapter" %*
