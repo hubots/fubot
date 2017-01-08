cad6fa7ecc0bf090d3b2755b15dea9d7cc468ce2
sergeyt <stodyshev@gmail.com>
2017-01-08 17:35:38 +0700

    fixing script

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,9 +1,8 @@
 @echo off
 set HUBOT_LOG_LEVEL=debug
-set HUBOT_CAMPFIRE_HOST=localhost
-set HUBOT_CAMPFIRE_PORT=3000
-set HUBOT_CAMPFIRE_APIPREFIX=api
-set HUBOT_CAMPFIRE_TOKEN=hubot:hub0t2015
-set HUBOT_CAMPFIRE_ACCOUNT=hubot
+set HUBOT_CHAT_HOST=localhost
+set HUBOT_CHAT_PORT=3000
+set HUBOT_CHAT_TOKEN=hubot:hub0t2015
+set HUBOT_CHAT_ACCOUNT=hubot
 
-npm install && node_modules\.bin\hubot.cmd --name "hubot" --adapter "campfire-adapter" %*
+npm install && node_modules\.bin\hubot.cmd --name "hubot" --adapter "hubot-custom-adapter" %*
