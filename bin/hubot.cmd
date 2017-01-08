69802020395944e557a2b72316bb2388ebbf72d8
sergeyt <stodyshev@gmail.com>
2017-01-08 23:01:35 +0700

    fix startup scripts

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -2,7 +2,7 @@
 set HUBOT_LOG_LEVEL=debug
 set HUBOT_CHAT_HOST=localhost
 set HUBOT_CHAT_PORT=3000
-set HUBOT_CHAT_TOKEN=hubot:hub0t2015
-set HUBOT_CHAT_ACCOUNT=hubot
+set HUBOT_CHAT_TOKEN=fubot:fub0t2017
+set HUBOT_CHAT_ACCOUNT=fubot
 
-npm install && node_modules\.bin\hubot.cmd --name "hubot" --adapter "hubot-custom-adapter" %*
+npm install && node_modules\.bin\hubot.cmd --name "hubot" --adapter "custom-adapter" %*
